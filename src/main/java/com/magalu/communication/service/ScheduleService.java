4827401e0d4ab499f1970aa7b05e41d5a75af36d
devleomeloo <leomelojesus@gmail.com>
2022-06-10 18:57:17 -0300

    Created Schedule POST Service

---
+++ src/main/java/com/magalu/communication/service/ScheduleService.java
@@ -0,0 +1,16 @@
+package com.magalu.communication.service;
+
+import com.magalu.communication.controller.ScheduleController;
+import com.magalu.communication.model.dto.ScheduleDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleService {
+
+    private ScheduleController scheduleController;
+
+    public ScheduleDTO create(ScheduleDTO scheduleDTO){
+        return //mapper
+    }
+
+}
